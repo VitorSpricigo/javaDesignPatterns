845c8484028f8e6665cb3f8863f899fca1f0c104
Vitor <vi_spricigo@hotmail.com>
2020-11-12 21:34:31 -0300

    Singleton desenvolvido

--- JavaDesignPatterns/src/Singleton/SingletonDemonstration.java
+++ JavaDesignPatterns/src/Singleton/SingletonDemonstration.java
@@ -1,5 +1,26 @@
 package Singleton;
 
-public class Singleton {
+public class SingletonDemonstration {
+	
+	public static void main(String[] args) {
+		
+		//Singleton singleton = new Singleton();
+		//Singleton singleton2 = new Singleton();
+		
+		Singleton singleton = Singleton.getInstance();
+		Singleton singleton2 = Singleton.getInstance();
+		
+		Integer i = 10;
+		Integer i2 = 20;
+		
+		singleton.setNum(10);
+		singleton2.setNum(20);
+
+		System.out.println("Singleton1: " + singleton.getNum());
+		System.out.println("Singleton2: " + singleton2.getNum());
+		
+		System.out.println(singleton.equals(singleton2));
+		System.out.println(i.equals(i2));
+	}
 
 }
